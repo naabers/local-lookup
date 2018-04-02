695bff1b2c6704bc8be3cc76c256abc00d09f808
Maome <steelereilly@gmail.com>
2018-04-02 19:12:47 -0400

    Add zkill, eve_xml, esi modules

---
+++ local_lookup/esi.py
@@ -0,0 +1,7 @@
+import requests
+
+def get_ship_name(ship_type_id):
+    url = "https://esi.tech.ccp.is/latest/universe/types/" + ship_type_id + "/"
+    search_results = requests.get(url)
+
+    return search_results.json()["name"]
