439990e0af3965332690c17ac019989f93657f05
Ray Naab <ray.naab@hotmail.com>
2018-08-23 19:45:55 -0700

    zkill is a bastard and changed the rules of their api

--- service/local_lookup/zkill.py
+++ service/local_lookup/zkill.py
@@ -10,17 +10,13 @@ from . import killmail
 def get_character_killmails(character_id_map):
     mails = []
 
-    #zkillboard requires the ids to be sorted for its api
-    character_ids = list(character_id_map)
-    character_ids.sort()
+    for character in character_id_map:
+        url = "https://zkillboard.com/api/characterID/" + str(character) + "/"
+        headers = {'Accept-Encoding': 'gzip', 'User-Agent': 'naabers'}
+        search_results = requests.get(url, headers=headers)
+        raw_mails = search_results.json()
 
-    character_id_str = ",".join(str(i) for i in character_ids)
-    url = "https://zkillboard.com/api/characterID/" + character_id_str + "/"
-    headers = {'Accept-Encoding': 'gzip', 'User-Agent': 'naabers'}
-    search_results = requests.get(url, headers=headers)
-    raw_mails = search_results.json()
-
-    for raw_mail in raw_mails:
-        mails.append(killmail.Killmail(raw_mail))
+        for raw_mail in raw_mails:
+            mails.append(killmail.Killmail(raw_mail))
 
     return mails
